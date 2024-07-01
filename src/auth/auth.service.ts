c145dd2a0ae1a7ce3d5dac8adf3b4a9bd39ee427
Roshan <1js20is089@gmail.com>
2024-07-01 22:19:34 +0530

    docker prisma setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return { mes: ' i ahve sign in' };
+  }
+
+  signin() {
+    return { mes: 'i  have sign in ' };
+  }
+}
