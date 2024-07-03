6cbcca563e2a5da3d1eb25143e7b4856b39f8a7f
Roshan <1js20is089@gmail.com>
2024-07-04 01:18:56 +0530

    validation by class validator

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
-export interface AuthDto {
+import { IsEmail, IsEmpty, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
